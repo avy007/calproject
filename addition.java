a5d9de404af06fda4c92746bc2cb05629c5281cf
akash <akashyewale007@gmail.com>
2023-02-14 23:02:06 +0530

    addition

---
+++ addition.java
@@ -0,0 +1,21 @@
+package DAY1;
+
+import java.util.Scanner;
+
+public class addition {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		System.out.println("enter nnumber: ");
+		
+		Scanner obj=new Scanner(System.in);
+		
+		a=obj.nextInt();
+		b=obj.nextInt();
+		 c=a+b;
+		 System.out.println("sum of two number: "+c);
+			
+
+	}
+
+}
